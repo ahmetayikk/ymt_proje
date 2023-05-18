33cdc7e7187b7beb5398457676ff736b95aa7cbd
Ahmet Ayık <112005268+ahmetayikk@users.noreply.github.com>
2023-05-19 01:10:50 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -1,9 +1,24 @@
 # Kullanıcıdan iki sayı alınır
-sayi1 = float(input("Birinci sayiyi girin: "))
-sayi2 = float(input("İkinci sayiyi girin: "))
+sayi1 = float(input("Birinci sayıyı girin: "))
+sayi2 = float(input("İkinci sayıyı girin: "))
 
-# İki sayıyı toplama işlemi yapar
-sonuc = sayi1 + sayi2
+# İşlem seçimi için kullanıcıya mesaj gösterilir
+print("İşlem seçin:")
+print("1 - Toplama")
+print("2 - Çıkarma")
+tercih = input("Tercihinizi yapın (1 veya 2): ")
+
+# İşlem tercihine göre hesaplama yapılır
+if tercih == "1":
+    sonuc = sayi1 + sayi2
+    islem = "toplama"
+elif tercih == "2":
+    sonuc = sayi1 - sayi2
+    islem = "çıkarma"
+else:
+    print("Geçersiz tercih!")
+    exit()
 
 # Sonucu ekrana yazdırma
-print("Toplam: ", sonuc)
+print(f"Sonuç: {sonuc} ({sayi1} ile {sayi2} {islem} sonucu)")
+
