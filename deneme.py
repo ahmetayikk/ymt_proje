fe6936686dea4beabb4a23b3386ae035aaf94d45
ahmetayikk <ahmetayikk03@gmail.com>
2023-05-19 01:09:58 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,9 @@
+# Kullanıcıdan iki sayı alınır
+sayi1 = float(input("Birinci sayiyi girin: "))
+sayi2 = float(input("İkinci sayiyi girin: "))
+
+# İki sayıyı toplama işlemi yapar
+sonuc = sayi1 + sayi2
+
+# Sonucu ekrana yazdırma
+print("Toplam: ", sonuc)
