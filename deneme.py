8a07e6d1331417aad4b34db1a90fb7c31d0294aa
Ahmet Ayık <112005268+ahmetayikk@users.noreply.github.com>
2023-05-19 01:11:26 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -6,7 +6,8 @@ sayi2 = float(input("İkinci sayıyı girin: "))
 print("İşlem seçin:")
 print("1 - Toplama")
 print("2 - Çıkarma")
-tercih = input("Tercihinizi yapın (1 veya 2): ")
+print("3 - Çarpma")
+tercih = input("Tercihinizi yapın (1, 2 veya 3): ")
 
 # İşlem tercihine göre hesaplama yapılır
 if tercih == "1":
@@ -15,10 +16,14 @@ if tercih == "1":
 elif tercih == "2":
     sonuc = sayi1 - sayi2
     islem = "çıkarma"
+elif tercih == "3":
+    sonuc = sayi1 * sayi2
+    islem = "çarpma"
 else:
     print("Geçersiz tercih!")
     exit()
 
 # Sonucu ekrana yazdırma
 print(f"Sonuç: {sonuc} ({sayi1} ile {sayi2} {islem} sonucu)")
+
 
