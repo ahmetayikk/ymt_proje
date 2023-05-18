67fc76227815cfe6c39f8539f0fdca39d6001444
Ahmet Ayık <112005268+ahmetayikk@users.noreply.github.com>
2023-05-19 01:58:45 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -8,7 +8,8 @@ print("1 - Toplama")
 print("2 - Çıkarma")
 print("3 - Çarpma")
 print("4 - Bölme")
-secim = int(input("Seçiminizi yapın (1-4): "))
+print("5 - Hangisi büyük?")
+secim = int(input("Seçiminizi yapın (1-5): "))
 
 # İşlemi gerçekleştirme ve sonucu gösterme
 if secim == 1:
@@ -23,5 +24,12 @@ elif secim == 3:
 elif secim == 4:
     sonuc = sayi1 / sayi2
     print("Bölme sonucu:", sonuc)
+elif secim == 5:
+    if sayi1 > sayi2:
+        print("Birinci sayı büyük.")
+    elif sayi2 > sayi1:
+        print("İkinci sayı büyük.")
+    else:
+        print("Girilen sayılar birbirine eşit.")
 else:
     print("Geçersiz bir seçim yaptınız.")
